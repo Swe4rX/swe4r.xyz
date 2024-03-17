fc685220be793dc6a892f34706efc3746fb224aa
Swe4r <botmail274@gmail.com>
2024-03-17 23:47:25 +0100

    add centered

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,21 @@
 </script>
 
 <Navigation></Navigation>
-	<div>
-		<slot/>
+<div class="wrapperOut">
+	<div class="wrapper">
+		<slot />
 	</div>
+</div>
+
+<style lang="postcss">
+	.wrapperOut {
+		width: 100vw;
+		place-items: center;
+		display: grid;
+	}
+	.wrapper {
+		width: 60vw;
+		display: grid;
+		place-self: center;
+	}
+</style>
