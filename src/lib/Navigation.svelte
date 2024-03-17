3d6cfaea2cdb9a285aeaac7b5eac0c2328e8cdd6
Swe4r <botmail274@gmail.com>
2024-03-17 17:59:23 +0100

    Updated Navigation component and app styles
    Modified the Navigation.svelte file to improve the look and feel of the navigation bar by adding a backdrop filter, adjusting the height, and changing the margin of unordered list items. Additionally, updated the app.pcss file to change the background image from a .png to a .jpg file for enhanced loading speed and performance.

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -12,9 +12,12 @@
 		display: flex;
 		width:100%;
 		z-index: 1000;
+		backdrop-filter: blur(30px);
 		background: transparent;
+		height: 3vw;
 		
 		& ul {
+			margin-top: 0.5vw;
 			display: flex;
 			flex-direction: row;
 			width: 60%;
@@ -22,7 +25,6 @@
 			
 			& li > a {
 				border: none;
-				
 				&:hover {
 					color: #f00;
 				}
